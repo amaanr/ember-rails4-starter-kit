0759ab2685066c982b3325cd69a6545dd7cf7fd7
Amaan Rattansi <amaan@sameboat.ca>
2013-08-02 16:35:42 -0400

    created new, create and destroy methods for user authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,27 +1,36 @@
 class SessionsController < Devise::SessionsController
+  # GET /resource/sign_in
+  def new
+    self.resource = resource_class.new(sign_in_params)
+    clean_up_passwords(resource)
+    respond_with(resource, serialize_options(resource))
+  end
+
   def create
     return missing_params unless params[:email] && params[:password]
 
-    build_resource
     resource = resource_from_credentials
     return invalid_credentials unless resource
 
-    resource.ensure_authentication_token!
+    # resource.ensure_authentication_token!
     data = {
       user_id: resource.id,
-      auth_token: resource.authentication_token,
+      # auth_token: resource.authentication_token,
     }
     render json: data, status: 201
   end
 
   def destroy
-    return missing_params unless params[:auth_token]
+    redirect_path = after_sign_out_path_for(resource_name)
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    set_flash_message :notice, :signed_out if signed_out && is_navigational_format?
 
-    resource = resource_class.find_by_authentication_token(params[:auth_token])
-    return invalid_credentials unless resource
-
-    resource.reset_authentication_token!
-    render json: {user_id: resource.id}, status: 200
+    # We actually need to hardcode this as Rails default responder doesn't
+    # support returning empty response on GET request
+    respond_to do |format|
+      format.all { head :no_content }
+      format.any(*navigational_formats) { redirect_to redirect_path }
+    end
   end
 
   protected
